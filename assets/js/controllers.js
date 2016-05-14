dfc27153b1d28921b9ab2dd14ccbd9fe7c16e4a7
TED <tedlade.tl@gmail.com>
2016-05-14 18:53:50 +0100

    Added search for doctors

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,101 +3,52 @@ angular.module('app.controllers', [])
 .controller('appCtrl', function($scope, Restangular) {
 	var inputMin = 1;
     $scope.search = function() {
-    	if ($scope.name && $scope.name.length >= inputMin) {
+    		console.log($scope.service)
+    	if ((($scope.name && $scope.name.length)>= inputMin ) && $scope.service == 'doctor') {
             $scope.searching = true;
             Restangular.one('doctor/search').get({name: $scope.name}).then(function(response){
             	$scope.searching = true;
                 $scope.results = response;
-                // console.log(response)
              }, function(error){
                 $scope.error = error;
             });
-        } else {
-               $scope.searching = false;
-          }     
-    }
-
-	$scope.showDetail = function(result) {
-		$scope.information = result;
-		console.log($scope.detail)
-		$scope.searching = false;
-		$scope.detail = true;
-	}
-
-	$scope.close = function() {
-		$scope.detail = false;
-	}
-
-})
-
-// .controller('dodgyDoctorsCtrl', function($scope, Restangular) {
-// 	// Restangular.all('doctor').getList().then(function(response) {
-		
-// 	// 	$scope.doctors = response;
-// 	// 	// console.log($scope.doctors.plain())
-// 	// })
-
-// 	var inputMin = 3;
-//     $scope.search = function() {
-//     	utilsFactory.fun1();
-//     	if ($scope.name && $scope.name.length >= inputMin) {
-//             $scope.searching = true;
-//             Restangular.one('doctor/search').get({name: $scope.name}).then(function(response){
-//                 $scope.results = response;
-//                 console.log(response)
-//              }, function(error){
-//                 $scope.error = error;
-//             });
-//         } else {
-//                $scope.searching = false;
-//           }     
-//     }
-
-// 	$scope.showDetail = function(result) {
-// 		$scope.information = result;
-// 		// console.log($scope.detail)
-// 		$scope.searching = false;
-// 		$scope.detail = true;
-// 	}
-
-// 	$scope.close = function() {
-// 		$scope.detail = false;
-// 	}
-// })
-
-.controller('medicineFinderCtrl', function($scope, Restangular) {
-	Restangular.all('medicine').getList().then(function(response) {
-		$scope.medicine = response;
-		// console.log(response.plain())  
-	})
-
-	var inputMin = 3;
-    $scope.search = function() {
-    	if ($scope.medicineName && $scope.medicineName.length >= inputMin) {
+        } else if ((($scope.name && $scope.name.length) >= inputMin) && $scope.service == 'hospital') {
             $scope.searching = true;
-            Restangular.one('medicine/search').get({name: $scope.medicineName}).then(function(response){
-                $scope.results = response;
-             }, function(error){
-                $scope.error = error;
-            });
+            console.log('medicine')
+            $http({
+				method: 'GET',
+			    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+			          address + '&key=AIzaSyCq_hPKdxEybYoangnVh1Fs_ARyjnmdSqs' + '&sensor=false&components=country:NG',
+
+			    transformRequest: function(data, headersGetter) {
+			        var headers = headersGetter();
+
+			        delete headers['Authorization'];
+
+			        return headers;
+			    }
+			}).then(function(results){
+				$scope.searching = true;
+			    $scope.results = results.data.results;
+			})
         } else {
-               $scope.searching = false;
-          }     
+        	$scope.searching = false;
+        }
     }
 
 	$scope.showDetail = function(result) {
-		$scope.information = result;
-		// console.log($scope.detail)
 		$scope.searching = false;
+		$scope.name = '';
 		$scope.detail = true;
-	}
-	$scope.showDetail = function() {
-		$scope.detail = true;
+		$scope.information = result;
+		console.log(result)
+		// $scope.searching = false;
 	}
 
 	$scope.close = function() {
 		$scope.detail = false;
 	}
+
 })
 
 .controller('hospitalFinderCtrl', function($scope, Restangular, $http) {
@@ -157,62 +108,3 @@ angular.module('app.controllers', [])
 		$scope.hospitalDetails = false;
 	}
 })
-
-// .factory('utilsFactory', function() {
-// 	//function definition
-// 	var fun1 = function() {
-//                 var morphSearch = document.getElementById( 'morphsearch' ),
-//                     input = morphSearch.querySelector( 'input.morphsearch-input' ),
-//                     ctrlClose = morphSearch.querySelector( 'span.morphsearch-close' ),
-//                     isOpen = isAnimating = false,
-//                     // show/hide search area
-//                     toggleSearch = function(evt) {
-//                         // return if open and the input gets focused
-//                         if( evt.type.toLowerCase() === 'focus' && isOpen ) return false;
-
-//                         var offsets = morphsearch.getBoundingClientRect();
-//                         if( isOpen ) {
-//                             classie.remove( morphSearch, 'open' );
-
-//                             // trick to hide input text once the search overlay closes 
-//                             // todo: hardcoded times, should be done after transition ends
-//                             if( input.value !== '' ) {
-//                                 setTimeout(function() {
-//                                     classie.add( morphSearch, 'hideInput' );
-//                                     setTimeout(function() {
-//                                         classie.remove( morphSearch, 'hideInput' );
-//                                         input.value = '';
-//                                     }, 300 );
-//                                 }, 500);
-//                             }
-                            
-//                             input.blur();
-//                         }
-//                         else {
-//                             classie.add( morphSearch, 'open' );
-//                         }
-//                         isOpen = !isOpen;
-//                     };
-
-//                 // events
-//                 input.addEventListener( 'focus', toggleSearch );
-//                 ctrlClose.addEventListener( 'click', toggleSearch );
-//                 // esc key closes search overlay
-//                 // keyboard navigation events
-//                 document.addEventListener( 'keydown', function( ev ) {
-//                     var keyCode = ev.keyCode || ev.which;
-//                     if( keyCode === 27 && isOpen ) {
-//                         toggleSearch(ev);
-//                     }
-//                 } );
-
-
-//                 /***** for demo purposes only: don't allow to submit the form *****/
-//                 morphSearch.querySelector( 'button[type="submit"]' ).addEventListener( 'click', function(ev) { ev.preventDefault(); } );
-//             }
-	
-// 	//return functions after the injection
-// 	return {
-// 	    fun1: fun1
-// 	};
-// })
